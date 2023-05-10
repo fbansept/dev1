3fdf806e2729695e435bf3b3dc86f579101d4a3c
fbansept <bansept.franck@gmail.com>
2023-05-10 16:22:25 +0200

    connexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthentificationService } from './services/authentification.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dev1';
 
-  constructor(public auth: AuthentificationService) {
-    
+  constructor(public auth: AuthentificationService, private router: Router) {}
+
+  onDeconnexion() {
+    this.auth.connecte = false;
+    this.router.navigateByUrl('/connexion');
   }
 }
--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthentificationService } from 'src/app/services/authentification.service';
 
 @Component({
@@ -10,9 +12,10 @@ import { AuthentificationService } from 'src/app/services/authentification.servi
 export class ConnexionComponent {
   constructor(
     private formBuilder: FormBuilder,
-    public auth: AuthentificationService
-  ) {
-  }
+    public auth: AuthentificationService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   formulaire: FormGroup = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
@@ -21,7 +24,18 @@ export class ConnexionComponent {
 
   onConnexion() {
     if (this.formulaire.valid) {
+      this.http
+        .post(
+          'http://localhost/backend-angular/connexion.php',
+          this.formulaire.value,
+          {
+            responseType: 'text'
+          }
+        )
+        .subscribe((reponse) => console.log(reponse));
+
       this.auth.connecte = true;
+      this.router.navigateByUrl('/accueil');
     }
   }
 }
